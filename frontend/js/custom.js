0cc7a1ac04ffaae1cfe9e4729eeab80322e51f19
Krzysztof Szydłowski <kriszna14@gmail.com>
2017-04-05 22:08:21 +0200

    layout fixes

---
+++ frontend/js/custom.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+    $(document).on('click', '#monitorSelector li', function() {
+        $('#monitorSelector li').removeClass('active');
+        $(this).addClass('active');
+    });
+});
