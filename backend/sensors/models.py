7cdd6e59b8319ffac98e38d52b3b591d6d308979
Krzysztof Skowronek <k.skowronek@mamoko.com>
2017-04-05 19:38:40 +0200

    Added braces to fields initialization
    Switched to built-in User model

--- backend/sensors/models.py
+++ backend/sensors/models.py
@@ -1,6 +1,4 @@
-import hashlib
-import uuid
-
+from django.contrib.auth.models import User
 from django.db import models
 
 
@@ -11,40 +9,14 @@ class SensorKind(models.Model):
 class Sensor(models.Model):
     hostname = models.CharField(max_length=64)
     kind = models.ForeignKey(SensorKind)
-    registered_at = models.DateTimeField
+    registered_at = models.DateTimeField()
 
 
 class Measurement(models.Model):
     sensor = models.ForeignKey(Sensor)
-    value = models.FloatField
-    measurement_time = models.DateTimeField
-    upload_time = models.DateTimeField
-
-
-class User(models.Model):
-    """ Class representing user in system."""
-    name = models.CharField(max_length=32)
-    password = models.CharField
-    salt = models.CharField
-    created_at = models.DateTimeField
-
-    def hash_password(self, password):
-        """ Returns hash of given password using self.salt. """
-        return hashlib.sha512(password + self.salt).hexdigest()
-
-    def password_match(self, password_to_check):
-        """ Checks if given password matches the one stored in database, useful in log-in logic """
-        return self.password == self.hash_password(password_to_check)
-
-    def set_password(self, password):
-        """ Sets new password for user """
-        self.salt = uuid.uuid4()
-        self.password = self.hash_password(password)
-
-    def __init__(self, password="password"):
-        """ Creates user with given password, which is automatically hashed. Default password is 'password'
-        """
-        self.set_password(password)
+    value = models.FloatField()
+    measurement_time = models.DateTimeField()
+    upload_time = models.DateTimeField()
 
 
 class ComplexMeasurement(models.Model):
@@ -60,8 +32,8 @@ class ComplexMeasurement(models.Model):
     """
     sensor = models.ForeignKey(Sensor, on_delete=models.SET_NULL, null=True)
     name = models.CharField(max_length=256)
-    begin = models.DateTimeField
-    end = models.DateTimeField
-    time_window = models.BigIntegerField
-    frequency = models.BigIntegerField
+    begin = models.DateTimeField()
+    end = models.DateTimeField()
+    time_window = models.BigIntegerField()
+    frequency = models.BigIntegerField()
     owner = models.ForeignKey(User)
