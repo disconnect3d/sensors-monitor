7617b88f884a67a19f01f9becc9d2e0c92ea0a44
mjar <jaroszynska.m@gmail.com>
2017-04-27 03:00:32 +0200

    Added serializers to models

--- backend/sensors/serializers.py
+++ backend/sensors/serializers.py
@@ -1,8 +1,40 @@
 from django.contrib.auth.models import User
 from rest_framework import serializers
+from .models import SensorKind, Host, Sensor, MeasurementValue, ComplexMeasurement
 
 
 class UserSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = User
-        fields = ('url', 'username', 'email', 'groups')
+        fields = ('id', 'url', 'username', 'email', 'groups')
+
+
+class SensorKindSerializer(serializers.HyperlinkedModelSerializer):
+    id = serializers.ReadOnlyField()
+    class Meta:
+        model = SensorKind
+        fields = ('id', 'kind_name',)
+
+
+class HostSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Host
+        fields = ('id', 'name',)
+
+
+class SensorSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Sensor
+        fields = ('id', 'host', 'kind', 'registered_at')
+
+
+class MeasurementValueSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = MeasurementValue
+        fields = ('id', 'sensor', 'value', 'measurement_time', 'upload_time', 'complex_id')
+
+
+class ComplexMeasurementSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = ComplexMeasurement
+        fields = ('id', 'sensor', 'name', 'begin', 'end', 'time_window', 'frequency', 'owner')
