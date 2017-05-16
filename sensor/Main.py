eeae4fdfa4513db9e3e07054a11f899daf7e9e87
Jacek Szyja <szyja.jacek@gmail.com>
2017-05-16 09:15:40 +0200

    Proper formatting

--- sensor/Main.py
+++ sensor/Main.py
@@ -8,19 +8,19 @@ import uuid
 
 # Parse arguments
 parser = OptionParser()
-parser.add_option("-n", "--name", dest="name", default=str(uuid.uuid1().urn),
+parser.add_option("-n", "--name", dest="name", default=str(uuid.uuid1().urn[4:]),
                   help="name this pc", metavar="NAME")
 (options, args) = parser.parse_args()
 
 
 memoryUse = psutil.virtual_memory()
-totalMemory = memoryUse.total
-availableMemory = memoryUse.available
+totalMemory = memoryUse.total/1024/1024
+availableMemory = memoryUse.available/1024/1024
 usedPercentMemory = memoryUse.percent
 memory = {
-    'total': memoryUse.total,
-    'available': memoryUse.available,
-    'usedPercent': memoryUse.percent
+    'Total memory [MB]': memoryUse.total,
+    'Available memory [MB]': memoryUse.available,
+    'Used memory [%]': memoryUse.percent
 }
 print(memory)
 
@@ -29,18 +29,36 @@ cpuPercent = psutil.cpu_percent(percpu=True)
 cores = {}
 counter = 0
 for cpu in cpuPercent:
-    cores['Core '+str(counter)] = cpu
+    cores['Core '+str(counter)+' [%]'] = cpu
     counter += 1
 print(cores)
 
-jsonData = {
-    'name': options.name,
-    'memory': memory,
-    'cpu': cores
+jsonBase = {
+    'host': options.name,
+    'registered_at': 's'
 }
 
-json = json.dumps(jsonData)
-print(json)
+measurementDate = str(datetime.utcnow())
+
+jsons = []
+for key, value in memory.items():
+    newJson = jsonBase.copy()
+    newJson["kind"] = key
+    newJson["values"] = [[measurementDate, value]]
+    jsons.append(newJson)
+
+for key, value in cores.items():
+    newJson = jsonBase.copy()
+    newJson["kind"] = key
+    newJson["values"] = [[measurementDate, value]]
+    jsons.append(newJson)
+
+for jsonData in jsons:
+    jsonString = json.dumps(jsonData)
+    print(jsonString)
+
+# json = json.dumps(jsonData)
+# print(json)
 
 
 # psutil.sensors_temperatures()
