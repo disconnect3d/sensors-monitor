48d389ee95b4ef250e546ee94fe142ee35119868
Kamill0 <kamillo1493@gmail.com>
2017-05-17 20:59:58 +0200

    Ajax method added

--- frontend/js/generic.js
+++ frontend/js/generic.js
@@ -32,6 +32,44 @@
         $("#dim").css("display", "none");
     }
 
+    function makeAjaxCall(hostname)
+    {
+        $.ajax({
+
+            url: hostname,
+
+            // The data to send (will be converted to a query string)
+            data: {
+                id: 123
+            },
+
+            crossDomain: true,
+
+            // Whether this is a POST or GET request
+            type: "GET",
+
+            // The type of data we expect back
+            dataType : "json",
+        })
+          // Code to run if the request succeeds (is done);
+          // The response is passed to the function
+          .done(function( json ) {
+             alert(json);
+          })
+          // Code to run if the request fails; the raw request and
+          // status codes are passed to the function
+          .fail(function( xhr, status, errorThrown ) {
+            alert( "Sorry, there was a problem!" );
+            console.log( "Error: " + errorThrown );
+            console.log( "Status: " + status );
+            console.dir( xhr );
+          })
+          // Code to run regardless of success or failure;
+          .always(function( xhr, status ) {
+            alert( "The request is complete!" );
+          });
+    }
+
 
 
 /*********************************
@@ -211,6 +249,8 @@
 
             afterSlideLoad: onSlideLoad
         });
+
+        makeAjaxCall("http://disconnect3d.pl:1337/sensors/")
 
         addMonitor();
         addSensors("hostname");
