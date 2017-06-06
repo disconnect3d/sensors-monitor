ae074509d1aec21c64e418f277cf5898e7c09a68
disconnect3d <dominik.b.czarnota@gmail.com>
2017-06-07 00:18:02 +0200

    Backend: add basic auth

--- backend/sensors/base_settings.py
+++ backend/sensors/base_settings.py
@@ -38,6 +38,16 @@ INSTALLED_APPS = [
     'sensors'
 ]
 
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+    ),
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
+    )
+}
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
